c6f69732747d4ba974f7181814a481f5ad62d2c0
Shin <tlsrhkdcjs12@naver.com>
2022-05-18 00:28:59 +0900

    Docker 파일 생성

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,22 @@
 module.exports = {
-  apps : [{
-    name: "backend",
-    script: "npm",
-    args: "run start:server",
-    cwd: "./back",
-  }, {
-    name: "frontend",
-    script: "serve",
-    args: "-s build",
-    cwd: "./front",
-  }]
-}
+  apps: [
+    // {
+    //   name: "backend",
+    //   script: "npm",
+    //   args: "run start:server",
+    //   cwd: "./back",
+    // },
+    {
+      name: "frontend",
+      script: "serve",
+      args: "-s build",
+      cwd: "./front",
+    },
+    {
+      script: "./back/index.js",
+      instances: "max",
+      exec_mode: "cluster",
+      max_memory_restart: "300M", // 프로세스의 메모리가 300MB에 도달하면 reload 실행
+    },
+  ],
+};
